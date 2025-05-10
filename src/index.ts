afed6c9db3b9ce7fd9d40c8452cb715557de11f5
sahiljamwal <sahil.jamwal78625@gmail.com>
2025-05-10 09:52:26 +0530

    refactor(master): update server initialization to use configuration and add MongoDB connection handling

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,18 @@
 import { createServer } from "http";
+import configuration from "./common/configurations/config";
 import app from "./app";
+import { createMongoConnection } from "./common/utils/db.util";
+import logger from "./common/utils/logger.util";
 
-const PORT = process.env.PORT || 3000;
+const PORT = configuration.port;
 const server = createServer(app);
 
-server.listen(PORT, () => console.log(`Server started at PORT ${PORT}`));
+server.listen(PORT, async () => {
+  try {
+    await createMongoConnection();
+    logger.info(`Server started at PORT ${PORT}`);
+  } catch (error) {
+    logger.error({ error });
+    process.exit(1);
+  }
+});
